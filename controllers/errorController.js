3935c38ad74be57770aa874c1f78a23b63601b37
Juan Torres <juan93git@gmail.com>
2025-02-20 17:32:17 +0100

    Add error handling for duplicate fields and validation errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -5,6 +5,18 @@ const handleCastErrorDB = err => {
   return new AppError(message, 400);
 };
 
+const handleDuplicateFieldsDB = err => {
+  const value = err.message.match(/(["'])(\\?.)*?\1/)[0];
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = err => {
+  const errors = Object.values(err.errors).map(el => el.message);
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -43,8 +55,10 @@ module.exports = (err, req, res, next) => {
     sendErrorDev(err, res);
   }
   else if (process.env.NODE_ENV === 'production') {
-    let error = { ...err, name: err.name };
+    let error = { ...err, name: err.name, message: err.message };
     if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === 'ValidationError') error = handleValidationErrorDB(error);
 
     sendErrorProd(error, res);
   }
