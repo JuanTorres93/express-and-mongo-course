dda2ce5fc9dec45a3dfa008140927eef3d65de33
Juan Torres <juan93git@gmail.com>
2025-02-20 17:46:02 +0100

    Add global error handling for uncaught exceptions and unhandled rejections

--- server.js
+++ server.js
@@ -2,6 +2,23 @@ const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 dotenv.config({ path: './config.env' });
 
+
+// This handles uncaught syncronous exceptions
+// Must be at the beginning for it yo be already
+// subscribed to the event before the exception is thrown
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT EXECPTION! 💥 Shutting down...');
+  console.log(err);
+  // Wait for the server to finish pending requests and close it
+  // Jonas says that it is not necessary to close the server
+  // because the process is going to terminate anyway
+  // I'm leaving it here just in case as commented lines
+
+  // server.close(() => {
+  process.exit(1)
+  // });
+})
+
 const app = require('./app');
 
 const DB = process.env.DATABASE.replace(
@@ -17,6 +34,20 @@ mongoose.connect(DB, {
 
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+// Every time there is an unhandled rejection, the process 
+// object emits an event called unhandledRejection.
+// Here we are listening for this event and handling
+// the unhandled rejection 
+// This handles uncaught syncronous exceptions
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err);
+  // Wait for the server to finish pending requests and close it
+  server.close(() => {
+    process.exit(1)
+  });
 });
