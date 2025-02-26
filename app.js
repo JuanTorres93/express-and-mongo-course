2e507bf988993bab1c9724201add58fea534e790
Juan Torres <juan93git@gmail.com>
2025-02-26 13:57:27 +0100

    Add helmet middleware for security and limit request body size

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const app = express();
 
@@ -10,10 +11,15 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 // GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
+// Limit requests from same IP
 const limiter = rateLimit({
     max: 100,
     windowMs: 60 * 60 * 1000, // 1 hour
@@ -21,9 +27,15 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
-app.use(express.json());
+// Body parser, reading data from body into req.body
+app.use(express.json({
+    limit: '10kb' // Limit the body size to 10kb
+}));
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// Test middleware
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     next();
