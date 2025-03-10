c8038bde031a8a07bbd588f529aecef08e73dc1c
Juan Torres <juan93git@gmail.com>
2025-03-10 13:47:15 +0100

    Add static method to calculate average and number of ratings for reviews

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema({
   review: {
@@ -46,6 +47,38 @@ reviewSchema.pre(/^find/, function (next) {
 
   next()
 });
+
+// DOC static method
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+  // this points to the model
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId },
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' },
+      },
+    },
+  ]);
+
+  console.log(stats);
+
+  await Tour.findByIdAndUpdate(tourId, {
+    ratingsQuantity: stats[0].nRating,
+    ratingsAverage: stats[0].avgRating,
+  });
+}
+
+reviewSchema.post('save', function () {
+  // this points to the current review
+
+  // this.constructor points to the model, so it can
+  // call the static method
+  this.constructor.calcAverageRatings(this.tour);
+})
 
 const Review = mongoose.model('Review', reviewSchema);
 
