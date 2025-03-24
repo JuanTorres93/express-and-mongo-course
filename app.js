c10a32684283a94104f77eba217bf9913e3559ba
Juan Torres <juan93git@gmail.com>
2025-03-24 13:40:44 +0100

    Implemented CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -28,6 +29,14 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // GLOBAL MIDDLEWARES
+// Implement CORS
+// CORS for simple requests: GET, POST
+app.use(cors()); // Access-Control-Allow-Origin *
+
+// CORS for complex requests
+// they include DELETE, PATCH, PUT, etc.
+app.options('*', cors());
+
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 
